0b9a2538c62bc31d00a425488a345390dd3e38f0
GamerGeek <sagebeorn@live.com>
2023-12-12 18:38:34 -0800

    xCalc Version 6
    Major Changes
    - Messages use the title bar rather than alert windows
    - Complete rewrite to input system. An input field replaces prompt windows and constants can be used for either or both numbers in a calculation.
    
    Minor Changes
    - There are two sets of read and write buttons, labeled x and y.
    - Conversion to and from decimal has been split into separate buttons.
    - "ans" button replaces random number between 0 and 1. This gives the last answer for ease of writing to x and y.
    - The modulo operation and random number generator are relabeled as "mod" and "?" respectively
    
    Technical changes
    - Since process IDs are no longer visible, the random number generator internal ID has been changed to "rng" from "random number between 0 and"
    - All user-changeable variables are one array. This has the visible side effect of being referred to as Numbers 0 through 3

--- x/calc/script.js
+++ x/calc/script.js
@@ -1,83 +1,84 @@
-var num1
-var result
-var originalBase
-var tempNum
-function Reset() {
-    num1 = prompt("First Number", "2");
-} 
-Reset();
-function GetOperation(op) {
-    var num2 = prompt("Second Number", "2");
+var num = [0,0,0,0];
+var result;
+var originalBase;
+var tempNum;
+var dyn = document.getElementById("dyn");
+var field = document.getElementById("field");
+var confirmButton = document.getElementById("confirmButton");
+var numRequested;
+var opRequested;
+function DoAlert(q) {
+	dyn.innerHTML = "<b>Calculator Experiment v6.0.0</b><br>" + q;
+} function DoPrompt(q) {
+	DoAlert(q);
+	return field.value;
+} function GetOperation(op) {
     if (op == "+") {
-	    result = Number(num1)+Number(num2);
+	    result = Number(num[0])+Number(num[1]);
     } else if (op == "−") {
-		result = Number(num1)-Number(num2);
+		result = Number(num[0])-Number(num[1]);
     } else if (op == "*") {
-		result = Number(num1)*Number(num2);
+		result = Number(num[0])*Number(num[1]);
     } else if (op == "/") {
-		result = Number(num1)/Number(num2);
+		result = Number(num[0])/Number(num[1]);
     } else if (op == "%") {
-		result = Number(num1)%Number(num2);
+		result = Number(num[0])%Number(num[1]);
     } else if (op == "^") {
-		result = Number(num1)**Number(num2);
+		result = Number(num[0])**Number(num[1]);
     } else if (op == "sqrt") {
-		result = Number(num1)**(1 / (Number(num2)));
+		result = Number(num[0])**(1 / (Number(num[1])));
     } else if (op == "log") {
-		result = Math.log(num1) / Math.log(num2);
+		result = Math.log(num[0]) / Math.log(num[1]);
     } else if (op == "%") {
-		result = Number(num1)%Number(num2);
+		result = Number(num[0])%Number(num[1]);
     } else if (op == "hyp") {
-		result = Math.hypot(num1, num2);
+		result = Math.hypot(num[0], num[1]);
     } else if (op == "average") {
-		result = (Number(num1) + Number(num2))/2;
-    } else if (op == "in base") {
-		if (num2 == "10") {
-			originalBase = prompt("Original Base", "10");
-			result = parseInt(num1, originalBase);
-		} else {
-		result = Number(num1).toString(num2);
-		}
+		result = (Number(num[0]) + Number(num[1]))/2;
+    } else if (op == "base") {
+		result = Number(num[0]).toString(num[1]);
+	} else if (op == "dec") {
+		result = parseInt(num[0], num[1]);
 	} else {
-		window.alert("Error: Unknown Operation");
-	} window.alert(num1 + " " + op + " " + num2 + " = " + result);
-	num1 = result;
-	window.alert("You can now do math with the result or press a blue button to start a new calculation");
-}
-function GetFactorial() {
-	tempNum = Number(num1);
+		DoAlert("Error: Unknown Operation");
+	} DoAlert(result);
+	num[0] = result;
+	//DoAlert("You can now do math with the result or press a blue button to start a new calculation");
+} function GetFactorial() {
+	tempNum = Number(num[0]);
 	for(i = tempNum - 1; i >= 1; i--) {
 		tempNum = tempNum * i;
 	}
-	window.alert(num1 + "! = " + tempNum);
+	DoAlert(num[0] + "! = " + tempNum);
 	return tempNum
 } function GetTriangle() {
-	tempNum = Number(num1);
+	tempNum = Number(num[0]);
 	for(i = tempNum - 1; i >= 1; i--) {
 		tempNum = tempNum + i;
 	}
 	return tempNum
 } function DegreesToRadians() {
-	return num1 * (Math.PI / 180);
+	return num[0] * (Math.PI / 180);
 } function RadiansToDegrees() {
-	return num1 / (Math.PI / 180);
+	return num[0] / (Math.PI / 180);
 } function RandomInteger() {
-	return Math.round(Math.random() * num1);
+	return Math.round(Math.random() * num[0]);
 } function CoTangent() {
-	return 1 / Math.tan(num1);
+	return 1 / Math.tan(num[0]);
 } function HyperbolicCoTangent() {
-	return 1 / Math.tanh(num1);
+	return 1 / Math.tanh(num[0]);
 } function ArcCoTangent() {
-	return Math.PI / 2 - Math.atan(num1);
+	return Math.PI / 2 - Math.atan(num[0]);
 } function HyperbolicArcCoTangent() {
-	return (Math.log((1/num1)+1) - Math.log(1-(1/num1))) / 2
+	return (Math.log((1/num[0])+1) - Math.log(1-(1/num[0]))) / 2
 } function Secant() {
-	return 1 / Math.cos(num1);
+	return 1 / Math.cos(num[0]);
 } function HyperbolicSecant() {
-	return 1 / Math.cosh(num1);
+	return 1 / Math.cosh(num[0]);
 } function CoSecant() {
-	return 1 / Math.sin(num1);
+	return 1 / Math.sin(num[0]);
 } function HyperbolicCoSecant() {
-	return 1 / Math.sinh(num1);
+	return 1 / Math.sinh(num[0]);
 } var functions = {
 	"!": GetFactorial,
 	"triangle": GetTriangle,
@@ -86,7 +87,7 @@ function GetFactorial() {
 	"ln": Math.log,
 	"e ^": Math.exp,
 	"round": Math.round,
-	"random number between 0 and": RandomInteger,
+	"rng": RandomInteger,
 	"sin": Math.sin,
 	"asin": Math.asin,
 	"tan": Math.tan,
@@ -112,37 +113,40 @@ function GetFactorial() {
 	"csch": HyperbolicCoSecant,
 }
 function GetFunction(op) {
-	result = functions[op](num1);
-	if (op != "!" && op != "abs") {
-		window.alert(op + " " + num1 + " = " + result);
-	} else if (op == "abs") {
-		window.alert("|" + op + "| = " + result)
-	} num1 = result;
-	window.alert("You can now do math with the result or press a blue button to start a new calculation");
-}
-var constants = {
-	"pi": Math.PI,
-	"e": Math.E,
-	"phi": 1.61803398874989484820,
-}
-function GetConstant(op) {
-	num1 = constants[op]
-	if (op == "?") {
-		num1 = Math.random();
+	if(num[0] == null) {
+		return
 	}
-	window.alert("First Number is Set to " + num1);
+	result = functions[op](num[0]);
+	DoAlert(result)
+	num[0] = result;
 }
-function SetNumberOne(set) {
-	num1 = set
-} function SetNumberTwo() {
-	num2 = set
+function RequestInput(numID, op) {
+	field.style.display = "block";
+	confirmButton.style.display = "block";
+	numRequested = numID;
+	opRequested = op;
+	DoAlert("Please input a value for Number " + numRequested);
 }
-var data = 0
-function WriteData() {
-	data = num1;
-	window.alert("Data value " + data + " stored.");
+function DoInput(type, value) {
+	if(num[0] == null) {
+		return
+	}
+	if (type == "c") {
+		num[numRequested] = value;
+	} else if (type == "f") {
+		num[numRequested] = field.value;
+	} else if (type == "v") {
+		num[numRequested] = num[value];
+	} else {
+		num[numRequested] = "ERROR"
+	}
+	//console.log(numRequested + " -> " + num[numRequested]);
+	if (opRequested != null) {
+		GetOperation(opRequested);
+	} else {
+		DoAlert("Number " + numRequested + " set to " + num[0]);
+	}
+	field.style.display = "none";
+	confirmButton.style.display = "none";
 }
-function ReadData() {
-	num1 = data
-	window.alert("First Number is Set to " + num1);
-}
+RequestInput(0);
